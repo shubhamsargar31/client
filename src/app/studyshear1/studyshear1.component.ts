165c869e3454a937fbe1c9ed2ae58169e06165fc
shubhamsaragar31 <shubhamsargar403@gmail.com>
2024-04-04 18:20:08 +0530

    table iteration and data pass to ts file

--- src/app/studyshear1/studyshear1.component.ts
+++ src/app/studyshear1/studyshear1.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { HeaderMergedComponent } from "../header-merged/header-merged.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-studyshear1',
     standalone: true,
     templateUrl: './studyshear1.component.html',
     styleUrl: './studyshear1.component.css',
-    imports: [HeaderMergedComponent]
+    imports: [HeaderMergedComponent,FormsModule,CommonModule]
 })
 export class Studyshear1Component {
+
+    tableData = [
+        {
+          type: "PDF",
+          description: "Description of the PDF whatever may be",
+          dateTime: "31/3/2024 8:00"
+        },
+        {
+            type: "PDF",
+            description: "Description of the PDF whatever may be",
+            dateTime: "32/3/2024 9:00"
+          },
+        
+      ];
+
+      fun(){
+        console.log(this.tableData)
+      }
+
+
 
 }
